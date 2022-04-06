b82b6eff312696c16fed2c475755bf935b473e1f
Your Name <abc@example.com>
2022-04-06 23:51:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
+
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yjiyu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("TourBooking");
+    const servicesCollection = database.collection("service");
+    const bookingCollection = database.collection("Booking");
+
+    // POST API
+    app.post("/addService", async (req, res) => {
+      const service = req.body;
+      const result = await servicesCollection.insertOne(service);
+      res.json(result);
+    });
+
+    // GET API
+    app.get("/service", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    // POST API user Booking
+    app.post("/booking", async (req, res) => {
+      const service = req.body;
+      console.log(service);
+      const result = await bookingCollection.insertOne(service);
+      res.json(result);
+    });
+    // GET user Booking
+    app.get("/booking/:email", async (req, res) => {
+      const email = { email: req.params.email };
+      console.log(email);
+      const result = await bookingCollection
+        .find({
+          userEmail: req.params.email,
+        })
+        .toArray();
+      res.send(result);
+    });
+
+    // DELETE Orders
+    // app.delete("/booking/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   console.log("id");
+    //   const query = { _id: ObjectId(id) };
+    //   const result = await bookingCollection.deleteOne(query);
+    //   res.json(result);
+    // });
+    // Update Orders
+    // app.put("/booking/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   console.log("id", id);
+    //   const update = req.body;
+    //   const filter = { _id: ObjectId(id) };
+    //   console.log(filter);
+    //   const options = {upseert : true}
+    //   const updateDoc ={
+    //     $set:{
+    //       state: update.state,
+    //     }
+    //   }
+    //   console.log("update", updateDoc);
+    //   const result = await  bookingCollection.updateOne(filter, updateDoc, options)
+    //   res.send(result)
+    // });
+
+    // // GET API
+    // app.get("/booking", async (req, res) => {
+    //   const cursor = bookingCollection.find({});
+    //   const services = await cursor.toArray();
+    //   res.send(services);
+    // });
+  } finally {
+    // await client.close()
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log("Hit Server", port);
+});
